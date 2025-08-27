cc3092af22524ba7d4cfb6a7e80efb2a59b96427
Shahar-Kabesa <61517666+shaharkabesa@users.noreply.github.com>
2025-08-27 20:36:56 +0300

    updated skill class
    adding getter and setter for cost

--- SmallCLIRPG/skill.cpp
+++ SmallCLIRPG/skill.cpp
@@ -11,3 +11,17 @@ void skill::set_skill_damage(int dmg) {
 int skill::get_skill_damage() {
 	return skill_damage;
 }
+
+void skill::set_mana_cost(int cost) {
+	mana_cost = cost;
+}
+int skill::get_mana_cost() {
+	return mana_cost;
+}
+
+void skill::set_stamina_cost(int cost) {
+	stamina_cost = cost;
+}
+int skill::get_skill_damage() {
+	return stamina_cost;
+}
--- SmallCLIRPG/skill.h
+++ SmallCLIRPG/skill.h
@@ -11,8 +11,13 @@ public:
 
 	int get_skill_damage();
 	void set_skill_damage(int dmg);
-
+	int get_mana_cost();
+	void set_mana_cost(int cost);
+	int get_stamina_cost();
+	void set_stamina_cost(int cost);
 
 private:
 	int skill_damage = 0;
+	int mana_cost = 0;
+	int stamina_cost = 0;
 };
